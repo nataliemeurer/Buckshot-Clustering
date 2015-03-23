f27dc919822264a3d66c17dd89afe28f7d73d5d0
Kevin Meurer <kevin.a.meurer@gmail.com>
2015-03-22 23:13:52 -0400

    Modify program to run full tests automatically.

--- main.py
+++ main.py
@@ -7,36 +7,55 @@ import settings as ENV
 import utils as util
 import cluster as c
 import buckshot as buck
+import datetime
 
+mergingCriterias = ["single-link"]*7 + ["complete-link"]*7 + ["centroid"]*7
+print mergingCriterias
+kValues = [3, 5, 8, 10, 15, 25, 50] * 3
+print kValues
+iterCount = 0
 
-# FILE READING
-data = arff.readArff(ENV.DATA_SRC)			# read our file and store the data
+while iterCount < (len(mergingCriterias) - 1):
+	ENV.K = kValues[iterCount]
+	ENV.MERGING_CRITERIA = mergingCriterias[iterCount]
 
-# PREPROCESSING
-fullData = processor.dataBin(data)			# create a processing bin to manipulate our data
-fullData.fillMissingValues()				# fill all missing values
-for attrName in ENV.REMOVED_ATTRS:
-	fullData.removeAttribute(attrName)		# if specific attributes have been requested to be removed, we do so here
-if ENV.REMOVE_OUTLIERS == True:
-	if ENV.REMOVE_ALL_OUTLIERS == True:		# if we need to remove all outliers, we do so here
-		fullData.removeAllOutliers()
-	else:
-		for attrName in ENV.REMOVED_OUTLIERS:
-			fullData.removeAttrOutliers(attrName)	# remove specific outliers
-fullData.normalizeContinuousVariables()		# Normalize all continuous variables using the method specified in settings
-entries = fullData.getDataAsEntries()		# convert all data points to the structure of an entry, a class I defined
+	# FILE READING
+	data = arff.readArff(ENV.DATA_SRC)			# read our file and store the data
 
-# CLUSTERING
-clusterDriver = buck.BuckshotClusters()		# create a cluster driver to do our clustering
-if ENV.USE_RANDOM_SAMPLE == True:			# select a random sample to use for the cluster
-	sample = []
-	count = 0
-	while count < ENV.SAMPLE_SIZE:
-		if ENV.SAMPLE_WITH_REPLACEMENT == True:
-			sample.append(util.chooseOneWithReplacement(entries))
+	# PREPROCESSING
+	fullData = processor.dataBin(data)			# create a processing bin to manipulate our data
+	fullData.fillMissingValues()				# fill all missing values
+	for attrName in ENV.REMOVED_ATTRS:
+		fullData.removeAttribute(attrName)		# if specific attributes have been requested to be removed, we do so here
+	if ENV.REMOVE_OUTLIERS == True:
+		if ENV.REMOVE_ALL_OUTLIERS == True:		# if we need to remove all outliers, we do so here
+			fullData.removeAllOutliers()
 		else:
-			sample.append(util.chooseOneWithoutReplacement(entries))
-		count += 1
-	clusterDriver.clusterEntries(sample)
-else:
-	clusterDriver.clusterEntries(entries)		# pass our data into the cluster to be buckshot clustered	
+			for attrName in ENV.REMOVED_OUTLIERS:
+				fullData.removeAttrOutliers(attrName)	# remove specific outliers
+	fullData.normalizeContinuousVariables()				# Normalize all continuous variables using the method specified in settings
+	entries = fullData.getDataAsEntries()				# convert all data points to the structure of an entry, a class I defined
+
+	# CLUSTERING
+	startTime = None
+	clusterDriver = buck.BuckshotClusters()		# create a cluster driver to do our clustering
+	if ENV.USE_RANDOM_SAMPLE == True:			# select a random sample to use for the cluster
+		sample = []
+		count = 0
+		while count < ENV.SAMPLE_SIZE:
+			if ENV.SAMPLE_WITH_REPLACEMENT == True:
+				sample.append(util.chooseOneWithReplacement(entries))
+			else:
+				sample.append(util.chooseOneWithoutReplacement(entries))
+			count += 1
+		startTime = datetime.datetime.now()
+		clusterDriver.clusterEntries(sample)
+	else:
+		startTime = datetime.datetime.now()
+		clusterDriver.clusterEntries(entries)		# pass our data into the cluster to be buckshot clustered	
+
+	endTime = datetime.datetime.now()
+	timeSpent = endTime - startTime
+	print "PROGRAM COMPLETED IN " + str(timeSpent.seconds) + " SECONDS"
+	iterCount += 1
+
--- settings.py
+++ settings.py
@@ -1,34 +1,34 @@
 # USED TO STORE GLOBAL / ENVIRONMENTAL VARIABLES
 
 # GENERAL SETTINGS
-PROGRESS_BAR = True 					# BOOLEAN: set whether a progress bar is used to show output.  Should be turned off when writing to files
+PROGRESS_BAR = True 							# BOOLEAN: set whether a progress bar is used to show output.  Should be turned off when writing to files
 
 
 # DATA SETTINGS
-DATA_SRC = './data/adult-big.arff'		# STRING: The path to the data file
+DATA_SRC = './data/adult-big.arff'				# STRING: The path to the data file
 
 
 # PREPROCESSOR SETTINGS
-FILL_WITH_CLASS_MODE = True 			# BOOLEAN: Determines whether the program fills missing values with the class mode or the overall mode
-CLASSIFIER_NAME = "class" 				# STRING: Name of presumed classifier
-REMOVE_OUTLIERS = True 					# BOOLEAN: Determines whether any outliers will be removed
-REMOVE_ALL_OUTLIERS = True  			# BOOLEAN: If set to true, removes outliers from all continuous variables
-REMOVED_OUTLIERS = ['age']				# LIST(strings): If remove all outliers, set to false, the list of attributes that will be scanned for outliers
-OUTLIER_ZSCORE_THRESHOLD = [-2.5, 2.5]	# LIST(float): Range in which the z-score must fall for it to not be considered an outlier
-NORMALIZATION_METHOD = "min-max"		# STRING: normalization method--serves as default in the normalize attribute function. Possible values: "z-score", "min-max"
-NORMALIZED_MIN = 0						# INT: minimum value used for min-max normalization
-NORMALIZED_MAX = 1  					# INT: maximum value used for min-max normalization
+FILL_WITH_CLASS_MODE = True 					# BOOLEAN: Determines whether the program fills missing values with the class mode or the overall mode
+CLASSIFIER_NAME = "class" 						# STRING: Name of presumed classifier
+REMOVE_OUTLIERS = True 							# BOOLEAN: Determines whether any outliers will be removed
+REMOVE_ALL_OUTLIERS = True  					# BOOLEAN: If set to true, removes outliers from all continuous variables
+REMOVED_OUTLIERS = ['age']						# LIST(strings): If remove all outliers, set to false, the list of attributes that will be scanned for outliers
+OUTLIER_ZSCORE_THRESHOLD = [-2.5, 2.5]			# LIST(float): Range in which the z-score must fall for it to not be considered an outlier
+NORMALIZATION_METHOD = "min-max"				# STRING: normalization method--serves as default in the normalize attribute function. Possible values: "z-score", "min-max"
+NORMALIZED_MIN = 0								# INT: minimum value used for min-max normalization
+NORMALIZED_MAX = 1  							# INT: maximum value used for min-max normalization
 REMOVED_ATTRS = ["fnlwgt:", "education-num:"]	# LIST(strings): name of the attributes to be removed
 
 
 # CLUSTERING SETTINGS
-USE_RANDOM_SAMPLE = True 				# BOOLEAN: Determines whether the algorithm will sample the whole data set or a fixed size
-SAMPLE_SIZE = 20000 					# INT: Sets the sample size that the algorithm will cluster, which is randomly sampled without replacement
-SAMPLE_WITH_REPLACEMENT = False 		# BOOLEAN: If true, samples data with replacement.  Otherwise, does not replace
-K = 12									# INT: number of clusters desired
-DISTANCE_MEASURE = "euclidian"			# STRING: formula used to measure distance, currently only supports euclidian
-MERGING_CRITERIA = "complete-link"		# MERGING_CRITERIA: single-link, complete-link, centroid, wards
-MAX_SIMILARITY_THRESHOLD = 5.0			# FLOAT: sets the maximum distance threshold to merge two clusters
+USE_RANDOM_SAMPLE = False 						# BOOLEAN: Determines whether the algorithm will sample the whole data set or a fixed size
+SAMPLE_SIZE = 20000 							# INT: Sets the sample size that the algorithm will cluster, which is randomly sampled without replacement
+SAMPLE_WITH_REPLACEMENT = False 				# BOOLEAN: If true, samples data with replacement.  Otherwise, does not replace
+K = 3											# INT: number of clusters desired
+DISTANCE_MEASURE = "euclidian"					# STRING: formula used to measure distance, currently only supports euclidian
+MERGING_CRITERIA = "single-link"				# MERGING_CRITERIA: single-link, complete-link, centroid, wards
+MAX_SIMILARITY_THRESHOLD = 5.0					# FLOAT: sets the maximum distance threshold to merge two clusters
 
 # VALIDATOR SETTINGS
 NUM_OF_FOLDS = 10
